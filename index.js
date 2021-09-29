af19455c85b081e3ea771a0bcfc92dd75f179ff8
infy404 <shrijankarki96@gmail.com>
2021-09-29 17:37:08 -0400

    Added get all images endpoint

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var SERVER_NAME = 'user-api'
-var PORT = 8000;
+var SERVER_NAME = 'images-api'
+var PORT = 5000;
 var HOST = '127.0.0.1';
 
 
 var restify = require('restify')
 
   // Get a persistence engine for the users
-  , usersSave = require('save')('users')
+  , imageSave = require('save')('images')
 
   // Create the restify server
   , server = restify.createServer({ name: SERVER_NAME})
@@ -14,8 +14,8 @@ var restify = require('restify')
   server.listen(PORT, HOST, function () {
   console.log('Server %s listening at %s', server.name, server.url)
   console.log('Resources:')
-  console.log(' /users')
-  console.log(' /users/:id')  
+  console.log(' /images')
+  console.log(' /images/:id')  
 })
 
 server
@@ -25,29 +25,29 @@ server
   // Maps req.body to req.params so there is no switching between them
   .use(restify.bodyParser())
 
-// Get all users in the system
-server.get('/users', function (req, res, next) {
+// Get all images in the system
+server.get('/images', function (req, res, next) {
 
   // Find every entity within the given collection
-  usersSave.find({}, function (error, users) {
+  imageSave.find({}, function (error, images) {
 
     // Return all of the users in the system
-    res.send(users)
+    res.send(images)
   })
 })
 
-// Get a single user by their user id
-server.get('/users/:id', function (req, res, next) {
+// Get a single image by the image id
+server.get('/image/:id', function (req, res, next) {
 
   // Find a single user by their id within save
-  usersSave.findOne({ _id: req.params.id }, function (error, user) {
+  imageSave.findOne({ _id: req.params.id }, function (error, user) {
 
     // If there are any errors, pass them to next in the correct format
     if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
 
-    if (user) {
+    if (image) {
       // Send the user if no issues
-      res.send(user)
+      res.send(image)
     } else {
       // Send 404 header if the user doesn't exist
       res.send(404)
@@ -55,25 +55,29 @@ server.get('/users/:id', function (req, res, next) {
   })
 })
 
-// Create a new user
-server.post('/users', function (req, res, next) {
+// Add a new image
+server.post('/images', function (req, res, next) {
 
   // Make sure name is defined
   if (req.params.name === undefined ) {
     // If there are any errors, pass them to next in the correct format
     return next(new restify.InvalidArgumentError('name must be supplied'))
   }
-  if (req.params.age === undefined ) {
+  if (req.params.url === undefined ) {
     // If there are any errors, pass them to next in the correct format
-    return next(new restify.InvalidArgumentError('age must be supplied'))
+    return next(new restify.InvalidArgumentError('Name of the image must be supplied'))
   }
-  var newUser = {
+  if (req.params.size == undefined){
+    return next(new restify.InvalidArgumentError('Size of the image must be provided'))
+  }
+  var newImage = {
 		name: req.params.name, 
-		age: req.params.age
+		age: req.params.url,
+    size: req.params.size
 	}
 
-  // Create the user using the persistence engine
-  usersSave.create( newUser, function (error, user) {
+  // Adding the new Image using the persistence engine
+  imageSave.create( newImage, function (error, user) {
 
     // If there are any errors, pass them to next in the correct format
     if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
